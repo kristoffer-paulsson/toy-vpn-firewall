97eed8b48626f512ffc23bd41790e2889721bc7f
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-03-19 22:49:23 +0100

    Another fix

--- app/src/main/java/com/example/toyvpn/Receiver.java
+++ app/src/main/java/com/example/toyvpn/Receiver.java
@@ -1,8 +1,5 @@
 package com.example.toyvpn;
 
-import static com.example.toyvpn.Sender.HEADER_SIZE;
-
-import com.example.toyvpn.tcpip.IpUtil;
 import com.example.toyvpn.tcpip.Packet;
 import com.example.toyvpn.tcpip.TCBStatus;
 
@@ -52,40 +49,6 @@ public class Receiver extends TcpHandler {
         }
     }
 
-    private void sendTcpPack(ConnectionHandler handler, byte flag, byte[] data) {
-        int dataLen = 0;
-        if (data != null) {
-            dataLen = data.length;
-        }
-        Packet packet = IpUtil.buildTcpPacket(handler.destinationAddress, handler.sourceAddress, flag,
-                handler.myAcknowledgementNum, handler.mySequenceNum, handler.packId);
-        handler.packId += 1;
-        ByteBuffer byteBuffer = ByteBuffer.allocate(HEADER_SIZE + dataLen);
-        //
-        byteBuffer.position(HEADER_SIZE);
-        if (data != null) {
-            if (byteBuffer.remaining() < data.length) {
-                System.currentTimeMillis();
-            }
-            byteBuffer.put(data);
-        }
-        //
-        packet.updateTCPBuffer(byteBuffer, flag, handler.mySequenceNum, handler.myAcknowledgementNum, dataLen);
-        byteBuffer.position(HEADER_SIZE + dataLen);
-
-        connectionManager.getRemoteInput().offer(new Triple<>(byteBuffer, handler.tag, Gateway.CONN_OP.SEND));
-
-        if ((flag & (byte) Packet.TCPHeader.SYN) != 0) {
-            handler.mySequenceNum += 1;
-        }
-        if ((flag & (byte) Packet.TCPHeader.FIN) != 0) {
-            handler.mySequenceNum += 1;
-        }
-        if ((flag & (byte) Packet.TCPHeader.ACK) != 0) {
-            handler.mySequenceNum += dataLen;
-        }
-    }
-
     public void doConnect(ConnectionHandler handler) throws Exception {
         //Log.i(TAG, String.format("tick %s", tick));
         //
@@ -109,51 +72,6 @@ public class Receiver extends TcpHandler {
         if (flushed) {
             //SelectionKey key1 = (SelectionKey) objAttrUtil.getAttr(socketChannel, "key");
             handler.selectionKey.interestOps(SelectionKey.OP_READ);
-        }
-    }
-
-    private boolean tryFlushWrite(ConnectionHandler handler) {
-        try {
-            ByteBuffer buffer = handler.remoteOutBuffer;
-            if (handler.destSocket.socket().isOutputShutdown() && buffer.remaining() != 0) {
-                sendTcpPack(handler, (byte) (Packet.TCPHeader.FIN | Packet.TCPHeader.ACK), null);
-                buffer.compact();
-                return false;
-            }
-            if (!handler.destSocket.isConnected()) {
-                //Log.i(TAG, "not yet connected");
-                SelectionKey key = handler.selectionKey;
-                int ops = key.interestOps() | SelectionKey.OP_WRITE;
-                key.interestOps(ops);
-                System.currentTimeMillis();
-                buffer.compact();
-                return false;
-            }
-            while (buffer.hasRemaining()) {
-                int n = 0;
-                n = handler.destSocket.write(buffer);
-                if (n > 4000) {
-                    System.currentTimeMillis();
-                }
-                //Log.i(TAG, String.format("tryFlushWrite write %s", n));
-                if (n <= 0) {
-                    //Log.i(TAG, "write fail");
-                    //
-                    SelectionKey key = handler.selectionKey;
-                    int ops = key.interestOps() | SelectionKey.OP_WRITE;
-                    key.interestOps(ops);
-                    System.currentTimeMillis();
-                    buffer.compact();
-                    return false;
-                }
-            }
-            buffer.clear();
-            if (!handler.upActive) {
-                handler.destSocket.shutdownOutput();
-            }
-            return true;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
--- app/src/main/java/com/example/toyvpn/Sender.java
+++ app/src/main/java/com/example/toyvpn/Sender.java
@@ -1,13 +1,11 @@
 package com.example.toyvpn;
 
-import com.example.toyvpn.tcpip.IpUtil;
 import com.example.toyvpn.tcpip.Packet;
 import com.example.toyvpn.tcpip.TCBStatus;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 
@@ -168,85 +166,6 @@ public class Sender extends TcpHandler {
         tryFlushWrite(handler);
         sendTcpPack(handler, (byte) Packet.TCPHeader.ACK, null);
         System.currentTimeMillis();
-    }
-
-    private void sendTcpPack(ConnectionHandler handler, byte flag, byte[] data) {
-        int dataLen = 0;
-        if (data != null) {
-            dataLen = data.length;
-        }
-        Packet packet = IpUtil.buildTcpPacket(handler.destinationAddress, handler.sourceAddress, flag,
-                handler.myAcknowledgementNum, handler.mySequenceNum, handler.packId);
-        handler.packId += 1;
-        ByteBuffer byteBuffer = ByteBuffer.allocate(HEADER_SIZE + dataLen);
-        //
-        byteBuffer.position(HEADER_SIZE);
-        if (data != null) {
-            if (byteBuffer.remaining() < data.length) {
-                System.currentTimeMillis();
-            }
-            byteBuffer.put(data);
-        }
-        //
-        packet.updateTCPBuffer(byteBuffer, flag, handler.mySequenceNum, handler.myAcknowledgementNum, dataLen);
-        byteBuffer.position(HEADER_SIZE + dataLen);
-
-        connectionManager.getRemoteInput().offer(new Triple<>(byteBuffer, handler.tag, Gateway.CONN_OP.SEND));
-
-        if ((flag & (byte) Packet.TCPHeader.SYN) != 0) {
-            handler.mySequenceNum += 1;
-        }
-        if ((flag & (byte) Packet.TCPHeader.FIN) != 0) {
-            handler.mySequenceNum += 1;
-        }
-        if ((flag & (byte) Packet.TCPHeader.ACK) != 0) {
-            handler.mySequenceNum += dataLen;
-        }
-    }
-
-    private boolean tryFlushWrite(ConnectionHandler handler) {
-        try {
-            ByteBuffer buffer = handler.remoteOutBuffer;
-            if (handler.destSocket.socket().isOutputShutdown() && buffer.remaining() != 0) {
-                sendTcpPack(handler, (byte) (Packet.TCPHeader.FIN | Packet.TCPHeader.ACK), null);
-                buffer.compact();
-                return false;
-            }
-            if (!handler.destSocket.isConnected()) {
-                //Log.i(TAG, "not yet connected");
-                SelectionKey key = handler.selectionKey;
-                int ops = key.interestOps() | SelectionKey.OP_WRITE;
-                key.interestOps(ops);
-                System.currentTimeMillis();
-                buffer.compact();
-                return false;
-            }
-            while (buffer.hasRemaining()) {
-                int n = 0;
-                n = handler.destSocket.write(buffer);
-                if (n > 4000) {
-                    System.currentTimeMillis();
-                }
-                //Log.i(TAG, String.format("tryFlushWrite write %s", n));
-                if (n <= 0) {
-                    //Log.i(TAG, "write fail");
-                    //
-                    SelectionKey key = handler.selectionKey;
-                    int ops = key.interestOps() | SelectionKey.OP_WRITE;
-                    key.interestOps(ops);
-                    System.currentTimeMillis();
-                    buffer.compact();
-                    return false;
-                }
-            }
-            buffer.clear();
-            if (!handler.upActive) {
-                handler.destSocket.shutdownOutput();
-            }
-            return true;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private static void closeUpStream(ConnectionHandler handler) {
